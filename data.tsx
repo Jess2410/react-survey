9fed4ac75e4d99b77248cbdc4c56bf016e9ffb48
Jess2410 <jess2410@hotmail.fr>
2022-12-16 16:00:50 +0100

    HTML CSS

---
+++ data.tsx
@@ -0,0 +1,44 @@
+export const data = {
+  projects: [
+    {
+      href: "http://youtu.be/XCHPT2Javg8",
+      name: "Deux Frères",
+    },
+    {
+      href: "http://youtu.be/ldueSz9ezLM",
+      name: "Sol-Ange",
+    },
+    {
+      href: "http://youtu.be/PikpxPzD7Jk",
+      name: "Dans mon rêve",
+    },
+    {
+      href: "https://youtu.be/PBjeeEjFGsg",
+      name: "Quelques Kms",
+    },
+    {
+      href: "https://youtu.be/aeZ-74VpWSA",
+      name: "Positif",
+    },
+    {
+      href: "https://youtu.be/0t6ghGasSmA",
+      name: "Ailleurs",
+    },
+    {
+      href: "https://youtu.be/uBihUMLQPMI",
+      name: "Trahi",
+    },
+    {
+      href: "https://youtu.be/UODbxq7h2Io",
+      name: "Censure",
+    },
+    {
+      href: "https://youtu.be/9ZioBq6ieQU",
+      name: "Sac Pailleté",
+    },
+    {
+      href: "https://youtu.be/F2njm6qB-To",
+      name: "Pleine Lune",
+    },
+  ],
+};
